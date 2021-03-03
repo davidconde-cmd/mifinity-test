ecc6cbe1a46c22b65b0f3afeaf3290b453f44ec6
davidconde-cmd <79993924+davidconde-cmd@users.noreply.github.com>
2021-03-03 17:33:19 +0000

    Project models and components generation
    Project models and components generation

--- mifinity-app/src/app/app.module.ts
+++ mifinity-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { AddContactComponent } from './contacts/add-contact/add-contact.component';
+import { ViewContactComponent } from './contacts/view-contact/view-contact.component';
+import { AddressContactComponent } from './contacts/address-contact/address-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ContactsComponent,
+    AddContactComponent,
+    ViewContactComponent,
+    AddressContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ mifinity-app/src/app/contacts/address-contact/address-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-address-contact',
+  templateUrl: './address-contact.component.html',
+  styleUrls: ['./address-contact.component.css']
+})
+export class AddressContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
